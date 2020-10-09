195a6d28a214dd93f52290519d9369682ac24839
Marco Mello <14339585+marmello77@users.noreply.github.com>
2020-10-09 20:17:57 -0300

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -30,8 +30,8 @@ library(lubridate)
 if(!file.exists("./data")){dir.create("./data")}
 
 #Download the zipped data from the course's website
-fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-download.file(fileUrl, destfile="./data/data.zip")
+fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileURL, destfile="./data/data.zip")
 
 #Unzip the file
 unzip(zipfile="./data/data.zip",exdir="./data")
